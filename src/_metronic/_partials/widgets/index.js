f5e1379b5edfd432066893ff255b626135e89722
Anand <anand21mn@gmail.com>
2020-08-03 16:39:45 +0800

    delete template dashboards

--- src/_metronic/_partials/widgets/index.js
+++ src/_metronic/_partials/widgets/index.js
@@ -9,9 +9,6 @@ export {ListsWidget4} from "./lists/ListsWidget4";
 export {ListsWidget8} from "./lists/ListsWidget8";
 export {ListsWidget9} from "./lists/ListsWidget9";
 
-// Stats
-export {StatsWidget11} from "./stats/StatsWidget11";
-export {StatsWidget12} from "./stats/StatsWidget12";
 
 // Advance tables
 export {AdvanceTablesWidget2} from "./advance-tables/AdvanceTablesWidget2";
